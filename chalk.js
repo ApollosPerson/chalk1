8b22b552b744b5f4965514216d168723750f5a9e
Clydezilla <sacredforgeaikido@gmail.com>
2017-10-31 22:03:10 -0500

    Add files via upload

---
+++ chalk.js
@@ -0,0 +1,8 @@
+const chalk = require('chalk');
+
+console.log(chalk.blue("Do you know how to catch a ") + chalk.hex('#ffffff')("POLAR BEAR") + chalk.blue('???'));
+console.log(chalk.blue("It's ") + chalk.hex('#ffffff')("SUPER") + chalk.blue(" easy!"));
+console.log(chalk.magentaBright("FIRST ") + chalk.magenta("You chop a biiiig hole in the ice."));
+console.log(chalk.cyanBright("NEXT ") + chalk.cyan("You put frozen peas around the edge."));
+console.log(chalk.greenBright("THEN ") + chalk.green("When the ") + chalk.whiteBright("Polar Bear ") + chalk.green("comes to take a ") + chalk.yellowBright("pea..."));
+console.log(chalk.redBright("You sneak up behind him and kick him in the ") + chalk.whiteBright("ICE HOLE!!!"));
